8a742838dcbf5851d1c87d2e3e45475f5bfebfb0
MrRxdney (Rodney) <89966787+RodneyDaVinci@users.noreply.github.com>
2021-12-01 11:17:59 +0100

    Hello World!

---
+++ Tri-Tryout/Programma1.py
@@ -0,0 +1,25 @@
+aantal = int(input("Hoe vaak moet Hello world? in de output komen? Kies uit 1, 3 of 7.: "))
+
+
+
+def helloWorld():
+ if aantal == 1:
+    print("1. Hello World!")
+
+helloWorld()
+
+
+def helloWorld():
+ if aantal == 3:
+    for i in range(3):
+        print("Hello World!")
+
+helloWorld()
+
+def helloWorld():
+ if aantal == 7:
+    for i in range(7):
+        print("Hello World!")
+
+helloWorld()
+
