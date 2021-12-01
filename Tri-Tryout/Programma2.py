70c2434fe87d691644a00e7883fb98e47ac2e817
MrRxdney (Rodney) <89966787+RodneyDaVinci@users.noreply.github.com>
2021-12-01 11:18:47 +0100

    Berekeningen

---
+++ Tri-Tryout/Programma2.py
@@ -0,0 +1,46 @@
+def addition():
+    number1 = 10
+    number2 = 12
+    a = number1 + number2
+    return print(number1 ,'+', number2 ,'=', a)
+    
+addition()
+
+def substraction():
+    number1 = 58
+    number2 = 34
+    a = number1 - number2
+    return print(number1 ,'-', number2 ,'=', a)
+
+substraction()
+
+def multiplication():
+    number1 = 6
+    number2 = 7
+    a = number1 * number2
+    return print(number1 ,'x', number2 ,'=', a)
+
+multiplication()
+
+def division():
+    number1 = 144
+    number2 = 12
+    a = number1 / number2
+    return print(number1 ,':', number2 ,'=', a)
+
+division()
+
+def increment():
+    number1 = 12
+    a = number1+1
+    return print(number1 ,'+ 1 =', a)
+
+increment()
+
+
+def decrement():
+    number1 = 34
+    a = number1-1
+    return print(number1 ,'- 1 =', a)
+
+decrement()
